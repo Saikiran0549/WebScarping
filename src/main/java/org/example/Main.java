79bc767b4b5306fbaa0ca665b9957fd09ea25805
Saikiran0549 <161238004+Saikiran0549@users.noreply.github.com>
2024-07-15 18:38:57 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,11 +16,10 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
 
-        //As per clients' request for confidentiality, I am using a sample open source website called : www.cochranelibrary.com
-        System.out.println("please choose a number you want to browse");
+        //As per clients' request for confidentiality, I am using a sample open source website called : www.themoviedb.org
+        System.out.println("please choose a topic you want to browse");
 
-        System.out.println("1.Allergy & intolerance  2.Blood disorders  3.Cancer  4.Child health 5.Complementary & alternative medicine 6.Consumer & communication strategies 7.Dentistry & oral health 8.Developmental, psychological and learning problems  9.Diagnosis. 10.Ear,  nose & throat. 11.Effective practice & health systems 12.Endocrine & metabolic 13.Eyes & vision 14.Gastroenterology & hepatology 15.Genetic disorders 16.Health & safety at work 17.Health professional education 18.Heart & circulation  19.infectious disease 20.insurance medicine 21.Kidney disease 22.Lungs & airways 23.Mental Health 24.Methodology 25.Neonatal care 26.Neurology 27.Orthopaedics & trauma 28.Pain & anaesthesia 29.Pregnancy & childbirth 30.Public health 31.Reproductive & sexual health 32.Rheumatology 33.Skin disorders 34.Tobacco, drugs & alcohol 35.Urology 36.Wounds");
-
+        System.out.println(".........");
         URLHandler.topicIndex = Integer.parseInt(reader.readLine())-1;
 
         Document homePage = URLHandler.initiateSession();
